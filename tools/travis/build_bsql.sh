5f44a29f31e7bf692261fcdaabcf0b3e9c84a0e6
Useroth <useroth@cock.li>
2020-07-12 13:38:21 +0200

    ......

--- tools/travis/build_bsql.sh
+++ tools/travis/build_bsql.sh
@@ -12,7 +12,7 @@ git checkout FETCH_HEAD
 
 mkdir -p artifacts
 cd artifacts
-export CXX=g++-7
+export CXX=g++-8
 # The -D will be unnecessary past BSQL v1.4.0.0
 cmake .. -DMARIA_LIBRARY=/usr/lib/i386-linux-gnu/libmariadb.so
 make
